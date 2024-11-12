caec6827029bee45a0f343bd9285ed438b8364ed
Ary-Asyari <aryasyari17@gmail.com>
2024-11-12 10:17:40 +0700

    test: testing cache data kapal geosat setiap 30 menit

--- lib/core/services/vessel_service.dart
+++ lib/core/services/vessel_service.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: non_constant_identifier_names, prefer_final_fields
 
 // import 'package:geotraking/core/models/kapal_member.dart';
+import 'dart:convert';
+
 import 'package:geotraking/core/models/member.dart';
 import 'package:geotraking/core/services/auth/authenticate_service.dart';
 import 'package:geotraking/core/services/connection/connection.dart';
@@ -140,13 +142,146 @@ class VesselService {
   }
 
   // fungsi get data kapal(vessel) admin(geosat)
+  // Future<List<Map<String, dynamic>>?> getDataKapalGeosat() async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   final timeZonePreferences = prefs.getString('SetTimezonePreferences');
+
+  //   var settings = Connection.getSettings();
+  //   var conn = await MySqlConnection.connect(settings);
+  //   var results = await conn.query('''
+  //     SELECT
+  //       m.id,
+  //       m.idfull,
+  //       m.sn,
+  //       m.imei,
+  //       m.name AS nama_kapal,
+  //       mc.name AS kategori,
+  //       mt.name AS type,
+  //       c.customer_name AS custamer,
+  //       COALESCE(m.timestamp, '0000-00-00 00:00:00') AS tgl_aktifasi,
+  //       COALESCE(m.msgTimestamp_GMT, m.timestamp, '0000-00-00 00:00:00') AS broadcast,
+  //       COALESCE(m.atp_start, '0000-00-00 00:00:00') AS atp_start,
+  //       COALESCE(m.atp_end, '0000-00-00 00:00:00') AS atp_end,
+  //       CASE
+  //         WHEN m.lat IS NULL OR m.lat = '' OR m.lat = 'None' THEN 0
+  //         ELSE m.lat
+  //       END AS lat,
+  //       CASE
+  //         WHEN m.lon IS NULL OR m.lon = '' OR m.lon = 'None' THEN 0
+  //         ELSE m.lon
+  //       END AS lon,
+  // 			COALESCE(m.heading, 0) AS heading,
+  // 			COALESCE(m.speed, 0) AS speed,
+  //       COALESCE(speed * 3.6, 0) AS speed_kmh,
+  //       COALESCE(speed * 1, 0) AS speed_kn,
+  //       COALESCE(speed * 0.51444, 0) AS speed_ms,
+  //       COALESCE(speed * 1.15078, 0) AS speed_mph,
+  //       CASE WHEN m.rpm1 IS NULL THEN 0 ELSE m.rpm1 END AS rpm1,
+  //       CASE WHEN m.rpm2 IS NULL THEN 0 ELSE m.rpm2 END AS rpm2,
+  //       m.powerstatus,
+  // 			COALESCE(m.externalvoltage, 0) AS externalvoltage
+  //     FROM
+  //       ai_mobile m
+  //       LEFT JOIN ai_mobile_category mc ON m.category_id = mc.id
+  //       LEFT JOIN ai_mobile_type mt ON m.type_id = mt.id
+  //       LEFT JOIN ai_customer_data c ON m.customer = c.id
+  //     WHERE
+  //       m.type_id IN (1, 2, 4, 5, 15)
+  //     ORDER BY
+  //       tgl_aktifasi DESC
+  //   ''');
+
+  //   List<Map<String, dynamic>> dataKapalGeosatList = [];
+
+  //   if (results.isNotEmpty) {
+  //     for (var row in results) {
+  //       DateTime originalTimestamp = DateTime.parse(row['tgl_aktifasi']);
+  //       DateTime originalBroadcast = DateTime.parse(row['broadcast']);
+  //       DateTime adjustedTimestamp;
+  //       DateTime adjustedBroadcast;
+
+  //       switch (timeZonePreferences) {
+  //         case 'UTC':
+  //           adjustedTimestamp = originalTimestamp.subtract(Duration(hours: 7));
+  //           adjustedBroadcast = originalBroadcast.subtract(Duration(hours: 7));
+  //           break;
+  //         case 'UTC+7':
+  //           adjustedTimestamp = originalTimestamp;
+  //           adjustedBroadcast = originalBroadcast;
+  //           break;
+  //         case 'UTC+8':
+  //           adjustedTimestamp = originalTimestamp.add(Duration(hours: 1));
+  //           adjustedBroadcast = originalBroadcast.add(Duration(hours: 1));
+  //           break;
+  //         case 'UTC+9':
+  //           adjustedTimestamp = originalTimestamp.add(Duration(hours: 2));
+  //           adjustedBroadcast = originalBroadcast.add(Duration(hours: 2));
+  //           break;
+  //         default:
+  //           adjustedTimestamp = originalTimestamp;
+  //           adjustedBroadcast = originalBroadcast;
+  //       }
+
+  //       dataKapalGeosatList.add({
+  //         'id': row['id'],
+  //         'idfull': row['idfull'],
+  //         'sn': row['sn'],
+  //         'imei': row['imei'],
+  //         'nama_kapal': row['nama_kapal'],
+  //         'kategori': row['kategori'],
+  //         'type': row['type'],
+  //         'custamer': row['custamer'],
+  //         // 'tgl_aktifasi': row['tgl_aktifasi'],
+  //         // 'broadcast': row['broadcast'],
+  //         'tgl_aktifasi': adjustedTimestamp.toIso8601String(),
+  //         'broadcast': adjustedBroadcast.toIso8601String(),
+  //         'lat': row['lat'],
+  //         'lon': row['lon'],
+  //         'speed': row['speed'],
+  //         'speed_kmh': row['speed_kmh'],
+  //         'speed_kn': row['speed_kn'],
+  //         'speed_ms': row['speed_ms'],
+  //         'speed_mph': row['speed_mph'],
+  //         'heading': row['heading'],
+  //         'rpm1': row['rpm1'],
+  //         'rpm2': row['rpm2'],
+  //         'powerstatus': row['powerstatus'],
+  //         'externalvoltage': row['externalvoltage'],
+  //         'atp_start': row['atp_start'],
+  //         'atp_end': row['atp_end'],
+  //       });
+  //     }
+  //   }
+
+  //   await conn.close();
+
+  //   return dataKapalGeosatList;
+  // }
+
   Future<List<Map<String, dynamic>>?> getDataKapalGeosat() async {
     final prefs = await SharedPreferences.getInstance();
     final timeZonePreferences = prefs.getString('SetTimezonePreferences');
+    final lastFetchTime = prefs.getInt('lastFetchTime');
+    final cachedData = prefs.getString('cachedDataKapalGeosat');
 
-    var settings = Connection.getSettings();
-    var conn = await MySqlConnection.connect(settings);
-    var results = await conn.query('''
+    try {
+      // Check if data is cached and if it's been fetched within the last 30 minutes
+      if (cachedData != null && lastFetchTime != null) {
+        final currentTime = DateTime.now().millisecondsSinceEpoch;
+        if (currentTime - lastFetchTime < 30 * 60 * 1000) {
+          // Debug print to indicate data is being used from cache
+          print("Debug: Loading data from SharedPreferences cache.");
+          return List<Map<String, dynamic>>.from(jsonDecode(cachedData));
+        }
+      }
+
+      // Debug print to indicate fetching new data from the database
+      print("Debug: Fetching new data from the database.");
+
+      // Fetch new data from the database
+      var settings = Connection.getSettings();
+      var conn = await MySqlConnection.connect(settings);
+      var results = await conn.query('''
       SELECT 
         m.id, 
         m.idfull, 
@@ -168,8 +303,8 @@ class VesselService {
           WHEN m.lon IS NULL OR m.lon = '' OR m.lon = 'None' THEN 0 
           ELSE m.lon 
         END AS lon,
-				COALESCE(m.heading, 0) AS heading,
-				COALESCE(m.speed, 0) AS speed, 
+        COALESCE(m.heading, 0) AS heading,
+        COALESCE(m.speed, 0) AS speed, 
         COALESCE(speed * 3.6, 0) AS speed_kmh,
         COALESCE(speed * 1, 0) AS speed_kn,
         COALESCE(speed * 0.51444, 0) AS speed_ms,
@@ -177,7 +312,7 @@ class VesselService {
         CASE WHEN m.rpm1 IS NULL THEN 0 ELSE m.rpm1 END AS rpm1,
         CASE WHEN m.rpm2 IS NULL THEN 0 ELSE m.rpm2 END AS rpm2,
         m.powerstatus, 
-				COALESCE(m.externalvoltage, 0) AS externalvoltage
+        COALESCE(m.externalvoltage, 0) AS externalvoltage
       FROM 
         ai_mobile m
         LEFT JOIN ai_mobile_category mc ON m.category_id = mc.id
@@ -189,71 +324,80 @@ class VesselService {
         tgl_aktifasi DESC
     ''');
 
-    List<Map<String, dynamic>> dataKapalGeosatList = [];
+      List<Map<String, dynamic>> dataKapalGeosatList = [];
 
-    if (results.isNotEmpty) {
-      for (var row in results) {
-        DateTime originalTimestamp = DateTime.parse(row['tgl_aktifasi']);
-        DateTime originalBroadcast = DateTime.parse(row['broadcast']);
-        DateTime adjustedTimestamp;
-        DateTime adjustedBroadcast;
+      if (results.isNotEmpty) {
+        for (var row in results) {
+          DateTime originalTimestamp = DateTime.parse(row['tgl_aktifasi']);
+          DateTime originalBroadcast = DateTime.parse(row['broadcast']);
+          DateTime adjustedTimestamp;
+          DateTime adjustedBroadcast;
 
-        switch (timeZonePreferences) {
-          case 'UTC':
-            adjustedTimestamp = originalTimestamp.subtract(Duration(hours: 7));
-            adjustedBroadcast = originalBroadcast.subtract(Duration(hours: 7));
-            break;
-          case 'UTC+7':
-            adjustedTimestamp = originalTimestamp;
-            adjustedBroadcast = originalBroadcast;
-            break;
-          case 'UTC+8':
-            adjustedTimestamp = originalTimestamp.add(Duration(hours: 1));
-            adjustedBroadcast = originalBroadcast.add(Duration(hours: 1));
-            break;
-          case 'UTC+9':
-            adjustedTimestamp = originalTimestamp.add(Duration(hours: 2));
-            adjustedBroadcast = originalBroadcast.add(Duration(hours: 2));
-            break;
-          default:
-            adjustedTimestamp = originalTimestamp;
-            adjustedBroadcast = originalBroadcast;
+          switch (timeZonePreferences) {
+            case 'UTC':
+              adjustedTimestamp =
+                  originalTimestamp.subtract(Duration(hours: 7));
+              adjustedBroadcast =
+                  originalBroadcast.subtract(Duration(hours: 7));
+              break;
+            case 'UTC+7':
+              adjustedTimestamp = originalTimestamp;
+              adjustedBroadcast = originalBroadcast;
+              break;
+            case 'UTC+8':
+              adjustedTimestamp = originalTimestamp.add(Duration(hours: 1));
+              adjustedBroadcast = originalBroadcast.add(Duration(hours: 1));
+              break;
+            case 'UTC+9':
+              adjustedTimestamp = originalTimestamp.add(Duration(hours: 2));
+              adjustedBroadcast = originalBroadcast.add(Duration(hours: 2));
+              break;
+            default:
+              adjustedTimestamp = originalTimestamp;
+              adjustedBroadcast = originalBroadcast;
+          }
+
+          dataKapalGeosatList.add({
+            'id': row['id'],
+            'idfull': row['idfull'],
+            'sn': row['sn'],
+            'imei': row['imei'],
+            'nama_kapal': row['nama_kapal'],
+            'kategori': row['kategori'],
+            'type': row['type'],
+            'custamer': row['custamer'],
+            'tgl_aktifasi': adjustedTimestamp.toIso8601String(),
+            'broadcast': adjustedBroadcast.toIso8601String(),
+            'lat': row['lat'],
+            'lon': row['lon'],
+            'speed': row['speed'],
+            'speed_kmh': row['speed_kmh'],
+            'speed_kn': row['speed_kn'],
+            'speed_ms': row['speed_ms'],
+            'speed_mph': row['speed_mph'],
+            'heading': row['heading'],
+            'rpm1': row['rpm1'],
+            'rpm2': row['rpm2'],
+            'powerstatus': row['powerstatus'],
+            'externalvoltage': row['externalvoltage'],
+            'atp_start': row['atp_start'],
+            'atp_end': row['atp_end'],
+          });
         }
-
-        dataKapalGeosatList.add({
-          'id': row['id'],
-          'idfull': row['idfull'],
-          'sn': row['sn'],
-          'imei': row['imei'],
-          'nama_kapal': row['nama_kapal'],
-          'kategori': row['kategori'],
-          'type': row['type'],
-          'custamer': row['custamer'],
-          // 'tgl_aktifasi': row['tgl_aktifasi'],
-          // 'broadcast': row['broadcast'],
-          'tgl_aktifasi': adjustedTimestamp.toIso8601String(),
-          'broadcast': adjustedBroadcast.toIso8601String(),
-          'lat': row['lat'],
-          'lon': row['lon'],
-          'speed': row['speed'],
-          'speed_kmh': row['speed_kmh'],
-          'speed_kn': row['speed_kn'],
-          'speed_ms': row['speed_ms'],
-          'speed_mph': row['speed_mph'],
-          'heading': row['heading'],
-          'rpm1': row['rpm1'],
-          'rpm2': row['rpm2'],
-          'powerstatus': row['powerstatus'],
-          'externalvoltage': row['externalvoltage'],
-          'atp_start': row['atp_start'],
-          'atp_end': row['atp_end'],
-        });
       }
+
+      await conn.close();
+
+      // Cache the fetched data and the current timestamp
+      prefs.setString('cachedDataKapalGeosat', jsonEncode(dataKapalGeosatList));
+      prefs.setInt('lastFetchTime', DateTime.now().millisecondsSinceEpoch);
+
+      return dataKapalGeosatList;
+    } catch (e) {
+      // Handle any errors and print the error for debugging
+      print("Error fetching data: $e");
+      return null;
     }
-
-    await conn.close();
-
-    return dataKapalGeosatList;
   }
 
   // fungsi get data kapal(vessel) admin(apn)
