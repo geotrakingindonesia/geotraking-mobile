ec63bd81c7c1a7ea335304b9e0c5e8b7a6f39214
Ary-Asyari <aryasyari17@gmail.com>
2024-11-25 15:16:22 +0700

    docs: percobaan untuk geochat kirim nembak api

--- lib/views/chat/components/tab_detail.dart
+++ lib/views/chat/components/tab_detail.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: use_build_context_synchronously, prefer_const_constructors, unused_element, unnecessary_null_comparison, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, unused_field, no_leading_underscores_for_local_identifiers, use_super_parameters
 
+import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:flutter/material.dart';
 import 'package:geotraking/core/components/app_back_button.dart';
 import 'package:geotraking/core/constants/app_colors.dart';
-import 'package:geotraking/core/constants/app_defaults.dart';
 
 class TabDetail extends StatefulWidget {
   final String vesselName;
@@ -20,10 +20,10 @@ class _TabDetailState extends State<TabDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.cardColor,
+      // backgroundColor: AppColors.cardColor,
+      backgroundColor: Color.fromARGB(255, 2, 21, 38),
       appBar: AppBar(
         leading: const AppBackButton(),
-        // title: Text('${widget.vesselName}'),
         title: Text('Chat ${widget.vesselName}'),
         titleTextStyle: Theme.of(context).textTheme.titleMedium?.copyWith(
               color: Colors.black,
@@ -31,122 +31,73 @@ class _TabDetailState extends State<TabDetail> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(AppDefaults.margin),
-                    padding: const EdgeInsets.all(AppDefaults.padding),
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 98, 182, 183),
-                      borderRadius: AppDefaults.borderRadius,
-                    ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
                     child: Column(
                       children: [
-                        // Align(
-                        //   alignment: Alignment.centerLeft,
-                        //   child: Row(
-                        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //     children: [
-                        //       Text(
-                        //         "${widget.vesselName ?? 'N/A'}",
-                        //         style: Theme.of(context)
-                        //             .textTheme
-                        //             .bodyLarge
-                        //             ?.copyWith(color: Colors.black),
-                        //       ),
-                        //     ],
-                        //   ),
-                        // ),
-                        // const SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Fitur ini di sediakan untuk kapal yang menggunakan Iridium Edge.',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium
-                                ?.copyWith(color: Colors.black54),
-                          ),
+                        BubbleSpecialThree(
+                          text: 'Please try and give some feedback on it!',
+                          color: Color(0xFF1B97F3),
+                          tail: true,
+                          textStyle:
+                              TextStyle(color: Colors.white, fontSize: 16),
                         ),
-                        const SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            // DateFormat('d MMM y (HH:mm a)')
-                            //     .format(widget.trouble.createAt!),
-                            'Monday, 30 Sep 2024 (11:29 AM)',
-                            style: const TextStyle(
-                                color: Colors.black54,
-                                fontStyle: FontStyle.italic),
-                          ),
+                        BubbleSpecialThree(
+                          text: 'Please try and give some feedback on it!',
+                          color: Colors.grey,
+                          tail: true,
+                          isSender: false,
                         ),
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
+          ),
+          MessageBar(
+            onSend: (_) => print(_),
           ),
         ],
       ),
-      bottomNavigationBar: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-          child: Row(
-            children: [
-              Expanded(
-                child: Form(
-                  // key: _key,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        // cursorColor: Colors.red,
-                        // controller: _responController,
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.done,
-                        // validator: (value) => Validators.required(value),
-                        // onSaved: (value) => _respon = value!,
-                        style: const TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                          labelText: 'Type a message',
-                          prefixIcon:
-                              const Icon(Icons.email, color: Colors.black),
-                          labelStyle: const TextStyle(color: Colors.black),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(40),
-                            borderSide: const BorderSide(color: Colors.black),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(40),
-                            borderSide: const BorderSide(color: Colors.black),
-                          ),
-                          filled: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(width: 12),
-              // if (widget.trouble.status != 2)
-                FloatingActionButton(
-                  onPressed: () {},
-                  backgroundColor: Colors.blue,
-                  child: const Icon(
-                    Icons.send,
-                    color: Colors.white,
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ),
+      // body: Padding(
+      //   padding: const EdgeInsets.only(top: 10),
+      //   child: Column(
+      //     children: [
+      //       Expanded(
+      //         child: SingleChildScrollView(
+      //           child: Column(
+      //             children: [
+      //               BubbleSpecialThree(
+      //                 text: 'Please try and give some feedback on it!',
+      //                 color: Color(0xFF1B97F3),
+      //                 tail: true,
+      //                 textStyle: TextStyle(color: Colors.white, fontSize: 16),
+      //               ),
+      //               BubbleSpecialThree(
+      //                 text: 'Please try and give some feedback on it!',
+      //                 color: Colors.grey,
+      //                 tail: true,
+      //                 isSender: false,
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      // bottomNavigationBar: SafeArea(
+      //   child: MessageBar(
+      //     onSend: (_) => print(_),
+      //   ),
+      // ),
     );
   }
 }
